cf40ebfcc2519ac3cd38486f4ae2b4fcaf56b060
James Howson <james.howson@justice.wa.gov.au>
2024-02-25 21:22:01 +0800

    Regenerate Angular TS API Client.

--- src/repo-ranger-app/src/app/generated/api/api.ts
+++ src/repo-ranger-app/src/app/generated/api/api.ts
@@ -1,9 +1,9 @@
-export * from './dependencies.service';
-import { DependenciesService } from './dependencies.service';
+export * from './dependency-instances.service';
+import { DependencyInstancesService } from './dependency-instances.service';
 export * from './projects.service';
 import { ProjectsService } from './projects.service';
 export * from './repositories.service';
 import { RepositoriesService } from './repositories.service';
 export * from './sources.service';
 import { SourcesService } from './sources.service';
-export const APIS = [DependenciesService, ProjectsService, RepositoriesService, SourcesService];
+export const APIS = [DependencyInstancesService, ProjectsService, RepositoriesService, SourcesService];
