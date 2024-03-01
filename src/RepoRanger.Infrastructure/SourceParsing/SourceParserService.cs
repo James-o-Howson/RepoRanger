f2448bd0c01b3496f47ca92caae2a9499404679b
James Howson <james.howson@justice.wa.gov.au>
2024-03-01 21:09:55 +0800

    Reformat

--- src/RepoRanger.Infrastructure/SourceParsing/SourceParserService.cs
+++ src/RepoRanger.Infrastructure/SourceParsing/SourceParserService.cs
@@ -14,17 +14,16 @@ namespace RepoRanger.Infrastructure.SourceParsing;
 internal sealed class SourceParserService : ISourceParserService
 {
     private readonly ParsingContext _context;
-    
+
+    private readonly ILogger<SourceParserService> _logger;
+
     private readonly IMediator _mediator;
     private readonly SourceParserOptions _options;
-    private readonly ILogger<SourceParserService> _logger;
     private readonly IRepositoryParser _repositoryParser;
-
-    private IEnumerable<SourceOptions> EnabledSourceOptions => _options.Sources.Where(s => s.Enabled);
 
     public SourceParserService(IEnumerable<ISourceFileParser> fileContentParsers,
         IOptions<SourceParserOptions> options,
-        ILogger<SourceParserService> logger, 
+        ILogger<SourceParserService> logger,
         IMediator mediator, IRepositoryParser repositoryParser)
     {
         ConcurrentBag<ISourceFileParser> sourceFileParsers = new(fileContentParsers);
@@ -36,55 +35,45 @@ internal sealed class SourceParserService : ISourceParserService
         _context = ParsingContext.Create(sourceFileParsers);
     }
 
+    private IEnumerable<SourceOptions> EnabledSourceOptions => _options.Sources.Where(s => s.Enabled);
+
     public async Task<IEnumerable<Source>> ParseAsync()
     {
-        var parseTasks = EnabledSourceOptions.Select(ParseSourceAsync);
+        IEnumerable<Task<Source>> parseTasks = EnabledSourceOptions.Select(ParseSourceAsync);
         return await Task.WhenAll(parseTasks);
     }
 
     private async Task<Source> ParseSourceAsync(SourceOptions sourceOptions)
     {
         _logger.LogInformation("Parsing Source {SourceName}", sourceOptions.Name);
-        
-        var source = await CreateOrUpdateSourceAsync(sourceOptions.Name, sourceOptions.Location);
-        
+
+        Source source = await CreateOrUpdateSourceAsync(sourceOptions.Name, sourceOptions.Location);
+
         _context.StartParsing(source.Id);
-        
-        var repositories = await ParseRepositoriesAsync(sourceOptions);
+
+        IEnumerable<Repository> repositories = await ParseRepositoriesAsync(sourceOptions);
         source.AddRepositories(repositories);
-        
+
         _context.StopParsing();
-        
+
         _logger.LogInformation("Finished Parsing Source {SourceName}", sourceOptions.Name);
         return source;
     }
-    
+
     private async Task<Source> CreateOrUpdateSourceAsync(string name, string location)
     {
-        var existing = await _mediator.Send(new GetSourceByNameQuery
-        {
-            Name = name
-        });
+        SourcePreviewDto? existing = await _mediator.Send(new GetSourceByNameQuery { Name = name });
 
         int id;
         if (existing is null)
         {
-            id = await _mediator.Send(new CreateSourceCommand
-            {
-                Name = name,
-                Location = location
-            });
-
+            id = await _mediator.Send(new CreateSourceCommand { Name = name, Location = location });
         }
         else
         {
-            id = await _mediator.Send(new UpdateSourceCommand
-            {
-                Id = existing.Id,
-                Location = existing.Location
-            });
+            id = await _mediator.Send(new UpdateSourceCommand { Id = existing.Id, Location = existing.Location });
         }
-        
+
         _logger.LogInformation("Repo Ranger Job Finished - Source created Id: {SourceId}", id);
 
         return Source.Create(name, location);
@@ -92,22 +81,21 @@ internal sealed class SourceParserService : ISourceParserService
 
     private async Task<IEnumerable<Repository>> ParseRepositoriesAsync(SourceOptions sourceOptions)
     {
-        var paths = sourceOptions.LocationInfo.GetGitDirectories();
-        
+        IEnumerable<string> paths = sourceOptions.LocationInfo.GetGitDirectories();
+
         return await Task.WhenAll(paths
             .Where(p => !sourceOptions.IsExcluded(p))
             .Select(ParseRepositoryAsync));
-
     }
-    
+
     private async Task<Repository> ParseRepositoryAsync(string repositoryPath)
     {
         _context.GitDirectory = new DirectoryInfo(repositoryPath);
-        
+
         _logger.LogInformation("Parsing Repository {RepositoryName}", _context.GitRepositoryName);
 
-        var repository = await _repositoryParser.ParseAsync(_context);
-        
+        Repository repository = await _repositoryParser.ParseAsync(_context);
+
         _logger.LogInformation("Finished Parsing Repository {RepositoryName}", _context.GitRepositoryName);
 
         return repository;
