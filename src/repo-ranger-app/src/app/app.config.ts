f8aefc38fff2317903dc15f226ec01a4746dda79
James Howson <james.howson@justice.wa.gov.au>
2024-03-18 21:55:41 +0800

    remove extra whitespace.

--- src/repo-ranger-app/src/app/app.config.ts
+++ src/repo-ranger-app/src/app/app.config.ts
@@ -1,6 +1,5 @@
 import { ApplicationConfig, LOCALE_ID } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient } from '@angular/common/http';
