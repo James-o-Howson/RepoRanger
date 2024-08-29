0fb682b7f6e60b5d599889cd03b1f9f869a3f316
James Howson <james.howson@justice.wa.gov.au>
2024-08-29 23:33:47 +0800

    Delete unused domain exception.

--- src/RepoRanger.Domain/PersistedEvents/PersistedEventRetryThresholdExceededException.cs
+++
@@ -1,5 +0,0 @@
-﻿namespace RepoRanger.Domain.PersistedEvents;
-
-internal sealed class PersistedEventRetryThresholdExceededException(int retryCount, int threshold)
-    : Exception($"Retry count of \"{retryCount}\" exceeds threshold of \"{threshold}\".");
-    
