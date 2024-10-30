d8c71fa84f7ee396c9eb0c69a6d8e5345f0e98e8
James Howson <james.howson@justice.wa.gov.au>
2024-10-30 19:59:10 +0800

    Add a IEnumerable batching extension.

---
+++ src/RepoRanger.Abstractions/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,25 @@
+﻿namespace RepoRanger.Abstractions.Extensions;
+
+public static class EnumerableExtensions
+{
+    public static IEnumerable<IEnumerable<TItem>> Batch<TItem>(this IEnumerable<TItem> source, int size)
+    {
+        TItem[]? bucket = null;
+        var count = 0;
+
+        foreach (var item in source)
+        {
+            bucket ??= new TItem[size];
+
+            bucket[count++] = item;
+            if (count != size) continue;
+
+            yield return bucket;
+
+            bucket = null;
+            count = 0;
+        }
+
+        if (bucket != null && count > 0) yield return bucket.Take(count);
+    }
+}
