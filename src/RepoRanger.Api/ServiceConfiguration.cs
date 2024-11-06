e16adb9521a9b35cd76f03341ff854c4e28c70cc
James Howson <james.howson@justice.wa.gov.au>
2024-11-06 15:43:40 +0800

    Reenable OpenAPI Document.

--- src/RepoRanger.Api/ServiceConfiguration.cs
+++ src/RepoRanger.Api/ServiceConfiguration.cs
@@ -21,12 +21,12 @@ internal static class ServiceConfiguration
         services.AddControllers();
         services.AddEndpointsApiExplorer();
         services.AddHealthChecks().AddDbContextCheck<ApplicationDbContext>();
-        // services.AddOpenApiDocument(settings =>
-        // {
-        //     settings.Title = "Repo Ranger API";
-        //     settings.Version = "v1";
-        //     settings.Description = "API for managing repositories in Repo Ranger.";
-        // });
+        services.AddOpenApiDocument(settings =>
+        {
+            settings.Title = "Repo Ranger API";
+            settings.Version = "v1";
+            settings.Description = "API for managing repositories in Repo Ranger.";
+        });
         services.AddHttpContextAccessor();
         services.AddMediatr();
         services.AddValidatorsFromAssemblies([CommandsAssembly.Assembly, QueriesAssembly.Assembly]);
